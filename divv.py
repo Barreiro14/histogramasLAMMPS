7c17bf7e70db3187991a993310668536429e13bb
enrik03 <enriquecasanova10@gmail.com>
2022-03-14 16:13:49 -0400

    Casi

--- divv.py
+++ divv.py
@@ -1,26 +1,20 @@
 
 def i_his():
     file = open("posicion6.xyz", "r")
-    filx = open("posicion6.xyz", "r")
+    
     x = [] 
     y = []
     cont = []
-    i = 0 
+    
     for line in file:
-       
+        i = 0
         if (line[0] == '2'):
-            i = 0
+            
             #print(float(line[2:6]))
             x.append(float(line[2:6]))
-            #print(x)
-            y = line[2:6]
-            
-            for lin in filx:
-                #x.append(float(line[2:6]))
-                if lin[2:6] == y:
-                   i += 1
-            cont.append(i)    
            
-    print(cont)        
-
-i_his()
+    my_dict = {i:x.count(i) for i in x}               
+                              
+    return my_dict     
+         
+    
--- histogram.py
+++ histogram.py
@@ -22,17 +22,20 @@ def i_his():
            #print()
     print(i)        
 
-i_his()
-
+#i_his()
+x = np.linspace(-4, 4, 100)
  #Numero de atomos tipo 2 (10% de las particulas totales) multiplicado por el max timestep
-data = np.linspace(0,7168)
-np.histogram(data, x)
+data = np.linspace(0,8000)
+data = np.random.randn(1000)
+
+#np.histogram(data, x)
 # Esta parte la copie de https://matplotlib.org/3.5.1/gallery/animation/animated_histogram.html#sphx-glr-gallery-animation-animated-histogram-py
 # Solo la modifique un poco 
 nx, _ = np.histogram(data, x)
 def prepare_animation(bar_container):
     def animate(frame_number):
         # simulate new data coming in
+        data = np.random.randn(1000)
         nx, _ = np.histogram(data, x)
         #ny, _ = np.histogram(data,y)
         #nz, _ = np.histogram(data,z)
@@ -47,7 +50,7 @@ def prepare_animation(bar_container):
 fig, ax = plt.subplots()
 _, _, bar_container = ax.hist(data, nx, lw=1,
                               ec="yellow", fc="green", alpha=0.5)
-ax.set_ylim(top=100)  # set safe limit to ensure that all data is visible.
+ax.set_ylim(top=55)  # set safe limit to ensure that all data is visible.
 
 ani = animation.FuncAnimation(fig, prepare_animation(bar_container), 50,
                               repeat=False, blit=True)
