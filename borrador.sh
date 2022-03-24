7de898939a0655b7009dccfaffecae32f9911ebd
Gabriel Barreiro <Barry@Barrys-MacBook-Air.local>
2022-03-24 10:09:08 -0400

    fix timeStepFinder

--- borrador.sh
+++ borrador.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rm fil_pos0 fil_pos1 pos0
+rm fil_pos0 fil_pos1 pos0 pos1 pos2 pos3 pos4 pos5 pos6 pos7
--- timeStepFinder.py
+++ timeStepFinder.py
@@ -3,15 +3,14 @@
 def timeStepFinder():
     file = open("posicion6.xyz", "r")
     i = 0
-    
-    
     for line in file:
-        f = open("pos{}".format(i), "a")
-        if line[0:4] == '11264': #number of atoms
+        f = open("pos{}".format(i), "w")
+        if line[0:5] == '11264': #number of atoms
             i = i + 1
-            break
-            
+            print("encontro la linea")
+            #break
         else:
+            #print(line[0:4])
             f.write(line)
             f.write("")
 
