9ef7cbd1892bcb42e768e50715aa6c3b0ac53c5b
Gabriel Barreiro <Barry@Barrys-MacBook-Air.local>
2022-03-24 10:21:05 -0400

    fix timestepfincer

--- timeStepFinder.py
+++ timeStepFinder.py
@@ -4,13 +4,14 @@ def timeStepFinder():
     file = open("posicion6.xyz", "r")
     i = 0
     for line in file:
-        f = open("pos{}".format(i), "w")
+        f = open("pos{}".format(i), "a")
         if line[0:5] == '11264': #number of atoms
             i = i + 1
             print("encontro la linea")
             #break
         else:
             #print(line[0:4])
+            print(line)
             f.write(line)
-            f.write("")
+            #f.write("")
 
